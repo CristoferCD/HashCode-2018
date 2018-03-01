294f8f59500e8035c48d4814973495bd5a8842d9
Cristofer <cristofercnsdmngz@live.com>
2018-03-01 19:18:32 +0100

    dsa

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
+
+    public static List<Ride> loadRoutes(String file) {
+        try {
+            List<String> lineas = Files.readAllLines(Paths.get(file));
+            // Parsear
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
