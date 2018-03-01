b04be54cb84a23803208d638a7761e711779cde2
Cristofer <cristofercnsdmngz@live.com>
2018-03-01 21:03:30 +0100

    Ejecuta

--- src/Car.java
+++ src/Car.java
@@ -4,14 +4,32 @@ import java.util.List;
 public class Car {
     int id;
     Point currentPosition;
-    Boolean enRoute;
     List<Integer> assignedRoutes;
+    Ride currentRoute = null;
+    int accumDistance = 0;
 
-
-    public Car(int id, Point currentPosition, Boolean enRoute) {
+    public Car(int id, Point currentPosition) {
         this.id = id;
         this.currentPosition = currentPosition;
-        this.enRoute = enRoute;
         this.assignedRoutes = new ArrayList<>();
+    }
+
+    public void move() {
+        accumDistance++;
+        if (accumDistance == currentRoute.distance()) {
+            currentPosition = currentRoute.end;
+            accumDistance = 0;
+            currentRoute = null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", currentPosition=" + currentPosition +
+                ", assignedRoutes=" + assignedRoutes +
+                ", currentRoute=" + currentRoute +
+                '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,44 +4,62 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-    public static int R, C, F, N, B, T;
+    public static int Rows, Columns, AmountOfCars, NumberOfRides, Bonus, T;
 
     public static void main(String[] args) {
-        List<Ride> routes = loadRoutes(args[1]);
+        List<Ride> routes = loadRoutes(args[0]);
         int currentT = 0;
         List<Car> cars = new ArrayList<>();
 
-        List<Ride> listaRides = loadRoutes("a_example.in");
-        for(Ride r : listaRides){
+        System.out.println("Cargados " + routes.size());
+        for(Ride r : routes){
             System.out.println("--"+r);
         }
-        System.out.println(R +" "+ C +" "+ F +" "+ N +" "+ B +" "+ T);
-        for (Car car : cars ) {
-            Optional<Ride> currentRoutes = routes.stream().filter((route) -> {
-                try {
-                    Helpers.realCost(car.currentPosition, route, currentT);
-                    return true;
-                } catch (Exception e) {
-                    return false;
-                }
-            }).sorted((r1, r2) -> {
-                try {
-                    return Helpers.realCost(car.currentPosition, r1, currentT) <
-                            Helpers.realCost(car.currentPosition, r2, currentT) ? -1 : 1;
-                } catch (Exception e) {
-                    return 0;
-                }
-            }).findFirst();
+        System.out.println(Rows +" "+ Columns +" "+ AmountOfCars +" "+ NumberOfRides +" "+ Bonus +" "+ T);
 
-            if (currentRoutes.isPresent()) {
-                Ride selectedRoute = currentRoutes.get();
-                car.assignedRoutes.add(selectedRoute.id);
-                car.enRoute = true;
-                //TODO: igual no borra
-                routes.remove(selectedRoute);
+        for (int i = 0; i < AmountOfCars; i++)
+            cars.add(new Car(i, new Point(0,0)));
+
+        for (currentT = 0; currentT < T; currentT++) {
+            // Mover todos los coches
+            cars.stream().filter((c) -> c.currentRoute != null).forEach(Car::move);
+
+            List<Car> availableCars = cars.stream().filter((c) -> c.currentRoute == null).collect(Collectors.toList());
+
+            for (Car car : availableCars) {
+                int finalCurrentT = currentT;
+                Optional<Ride> currentRoutes = routes.parallelStream().filter((route) -> {
+                    try {
+                        Helpers.realCost(car.currentPosition, route, finalCurrentT);
+                        return true;
+                    } catch (Exception e) {
+                        return false;
+                    }
+                }).sorted((r1, r2) -> {
+                    try {
+                        return Helpers.realCost(car.currentPosition, r1, finalCurrentT) <
+                                Helpers.realCost(car.currentPosition, r2, finalCurrentT) ? -1 : 1;
+                    } catch (Exception e) {
+                        return 0;
+                    }
+                }).findFirst();
+
+                if (currentRoutes.isPresent()) {
+                    Ride selectedRoute = currentRoutes.get();
+                    car.assignedRoutes.add(selectedRoute.id);
+                    car.currentRoute = selectedRoute;
+                    //TODO: igual no borra
+                    routes.remove(selectedRoute);
+                }
+                System.out.println(car);
+
+                if (routes.isEmpty())
+                    return;
             }
         }
     }
@@ -51,28 +69,27 @@ public class Main {
             List<Ride> listaRides= new ArrayList<>();
             List<String> lineas = Files.readAllLines(Paths.get(file));
             String linea[]=lineas.get(0).split(" ");
-            R=Integer.parseInt(linea[0].trim());
-            C=Integer.parseInt(linea[1].trim());
-            F=Integer.parseInt(linea[2].trim());
-            N=Integer.parseInt(linea[3].trim());
-            B=Integer.parseInt(linea[4].trim());
-            T=Integer.parseInt(linea[5].trim());
+            Rows = Integer.parseInt(linea[0].trim());
+            Columns = Integer.parseInt(linea[1].trim());
+            AmountOfCars = Integer.parseInt(linea[2].trim());
+            NumberOfRides = Integer.parseInt(linea[3].trim());
+            Bonus = Integer.parseInt(linea[4].trim());
+            T = Integer.parseInt(linea[5].trim());
             for(int i=1;i<lineas.size();i++){
                 linea=lineas.get(i).split(" ");
                 Point inicio = new Point(Integer.parseInt(linea[0].trim()),Integer.parseInt(linea[1].trim()));
                 Point fin = new Point(Integer.parseInt(linea[2].trim()),Integer.parseInt(linea[3].trim()));
                 int eS = Integer.parseInt(linea[4].trim());
                 int lF = Integer.parseInt(linea[5].trim());
-                Ride r = new Ride(inicio, fin, eS, lF);
+                Ride r = new Ride(i, inicio, fin, eS, lF);
                 System.out.println(r);
                 listaRides.add(r);
             }
             return listaRides;
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
+            return new ArrayList<>();
         }
-        return new ArrayList<>();
     }
 }
 
