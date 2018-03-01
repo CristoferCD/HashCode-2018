fd443dc901250ac93a05ad96c59c9b9c65fdd171
Cristofer <cristofercnsdmngz@live.com>
2018-03-01 23:48:13 +0100

    Final

--- src/Main.java
+++ src/Main.java
@@ -73,7 +73,7 @@ public class Main {
       }
     }
       try {
-          PrintWriter writer = new PrintWriter("e_high_bonus.txt", "UTF-8");
+          PrintWriter writer = new PrintWriter("c_no_hurry.txt", "UTF-8");
           for (Car c : cars) {
               writer.println(c.printFormatted());
           }
